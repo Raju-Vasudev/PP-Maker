a47167196a07ce48a278f6f31e0fc660435fce1a
Rj <raju.az@hotmail.com>
2025-12-10 04:18:36 +0530

    fix

--- pp-creator/vite.config.ts
+++ pp-creator/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Photo-maker/", // This is your GitHub repository name and subfolder
+  base: "/PP-maker/", // This is your GitHub repository name and subfolder
 })
 
